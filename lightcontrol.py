65723acbac01fae26dc9ead28de84c4d8904c166
Sharon Talbot <sharon.talbot@gmail.com>
2011-11-11 00:44:24 -0800

    check the temperature with noaa and update the led color as appropriate.

--- lightcontrol.py
+++ lightcontrol.py
@@ -1,17 +1,34 @@
+from decimal import *
 import serial
 import time
 import sys
+import  pywapi
+import string
 
 blinkTime = 1
-numBlinks = int(sys.argv[1])
+print sys.argv.count
+if len(sys.argv) > 1:
+    callInterval = int(sys.argv[1])
+else:
+    callInterval = 10
+
+if len(sys.argv) > 2:
+    numBlinks = int(sys.argv[2])
+else:
+    numBlinks = 20
+if len(sys.argv) > 3:
+    weatherStation = sys.argv[3]
+else:
+    weatherStation = 'KRNT'
 s = serial.Serial("/dev/tty.usbmodem12341")
 colorTime = 10
+noaa_result = pywapi.get_weather_from_noaa('KJFK')
 
 def shiftColor(color):
     s.write('H')
     s.write(color)
-    time.sleep(colorTime)
-    s.write('L')
+    #time.sleep(colorTime)
+    #s.write('L')
 
 def blinkenLights(num):
     for x in range(num):
@@ -19,14 +36,37 @@ def blinkenLights(num):
         time.sleep(blinkTime)
         s.write('L')
         time.sleep(blinkTime)
+
+def temperatureCallCycle():
+    lastTemp = 10
+    while 1:
+      noaa_result = pywapi.get_weather_from_noaa(weatherStation)
+      temp = Decimal(noaa_result['temp_f'])
+      if lastTemp != temp:
+        if (temp < 40):
+            print temp 
+            print ' B\n'
+            shiftColor('B')
+        elif (temp < 70):
+            print temp
+            print ' G\n'
+            shiftColor('G')
+        else:
+            print temp
+            print ' R\n'
+            shiftColor('R')
+      lastTemp = temp
+      time.sleep(callInterval)
+
+def ExerciseLEDTests():
+    shiftColor('R')
+    time.sleep(1)
+    shiftColor('G')
+    time.sleep(1)
+    shiftColor('B')
+    time.sleep(1)
     
+    blinkenLights(numBlinks)
 
-shiftColor('R')
-time.sleep(1)
-shiftColor('G')
-time.sleep(1)
-shiftColor('B')
-time.sleep(1)
-
-blinkenLights(numBlinks)
+temperatureCallCycle()
 s.close()
